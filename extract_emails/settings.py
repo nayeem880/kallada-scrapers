40b03b542f599dfa6c4be4605ab885ded2245b2f
nayeem880 <mhnayem289@gmail.com>
2021-04-20 20:43:12 +0600

    modification setting

--- extract_emails/settings.py
+++ extract_emails/settings.py
@@ -11,15 +11,16 @@ NEWSPIDER_MODULE = 'extract_emails.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 256
+CONCURRENT_REQUESTS = 512
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+# DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
+# CONCURRENT_REQUESTS_PER_DOMAIN = 1
 # CONCURRENT_REQUESTS_PER_IP = 16 doest support by priority queue
+
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
